51209194e62cec8b7e65d6ca052320b99445492d
dynamo63 <smoulongui@gmail.com>
2021-05-06 16:49:56 +0100

    hotfix signals.py

--- users/signals.py
+++ users/signals.py
@@ -1,6 +1,6 @@
 from django.db.models.signals import post_save
 from django.dispatch import receiver
-from .models import SBFMember, Assignement
+from .models import SBFMember, Assignement, Codes
 from .utils import get_code_parrain
 
 @receiver(post_save, sender=SBFMember)
